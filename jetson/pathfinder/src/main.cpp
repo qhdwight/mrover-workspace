74fad92e58648d47f09eb38254bd1d4ff95be7e9
Quintin <qhdwight@gmail.com>
2022-03-12 22:30:30 -0500

    Parse ply files

--- jetson/pathfinder/src/main.cpp
+++ jetson/pathfinder/src/main.cpp
@@ -1,8 +1,13 @@
 #include <fstream>
+#include <string>
+#include <vector>
 #include <memory>
 #include <filesystem>
 #include <iostream>
+
 #include <tinyply.h>
+#include <rapidjson/document.h>
+#include <rapidjson/istreamwrapper.h>
 
 using namespace std;
 
@@ -14,28 +19,60 @@ template<typename T>
 vector<T> extract(tinyply::PlyFile& file, shared_ptr<tinyply::PlyData> data) {
     vector<T> vector(data->count);
     size_t size = data->buffer.size_bytes();
-    std::memcpy(vector.data(), data->buffer.get(), size);
+    memcpy(vector.data(), data->buffer.get(), size);
     return vector;
 }
 
-int main() {
-    try {
-        tinyply::PlyFile file;
-        filesystem::path path("./clouds/0.ply");
-        ifstream fileStream(path, ios::binary);
-        file.parse_header(fileStream);
-        shared_ptr<tinyply::PlyData> vertexData, labelData;
-        try {
-            vertexData = file.request_properties_from_element("vertex", {"x", "y", "z"});
-            labelData = file.request_properties_from_element("vertex", {"label"});
-        }
-        catch (exception const& e) {
-            cerr << "request from ply exception: " << e.what() << endl;
-        }
-        file.read(fileStream);
+rapidjson::Document readConfig() {
+    auto path = filesystem::current_path() / "config.json";
+    ifstream ifs(path);
+    if (!ifs.is_open())
+        throw runtime_error("Could not open config at " + path.string());
 
-        vector<float3> vertices = extract<float3>(file, vertexData);
-        vector<uint8_t> labels = extract<uint8_t>(file, labelData);
+    rapidjson::IStreamWrapper isw(ifs);
+    rapidjson::Document doc;
+    doc.ParseStream(isw);
+    return doc;
+}
+
+int main(int argc, char** argv) {
+    try {
+        vector<string> args(argv, argv + argc);
+        rapidjson::Document config = readConfig();
+
+        filesystem::path plyDir(config["ply_dir"].GetString());
+
+        if (!filesystem::exists(plyDir))
+            throw runtime_error("Could not find ply directory at " + plyDir.string());
+
+        for (auto const& dirEntry: filesystem::recursive_directory_iterator(plyDir)) {
+            filesystem::path path = dirEntry.path();
+            if (path.extension() != ".ply") {
+                continue;
+            }
+
+            cout << path.string() << endl;
+
+            ifstream fileStream(path, ios::binary);
+            if (!fileStream.is_open())
+                throw runtime_error("Could not open ply file at " + path.string());
+
+            tinyply::PlyFile file;
+            file.parse_header(fileStream);
+
+            shared_ptr<tinyply::PlyData> vertexData, labelData;
+            try {
+                vertexData = file.request_properties_from_element("vertex", {"x", "y", "z"});
+                labelData = file.request_properties_from_element("vertex", {"label"});
+            }
+            catch (exception const& e) {
+                cerr << "request from ply exception: " << e.what() << endl;
+            }
+            file.read(fileStream);
+
+            vector<float3> vertices = extract<float3>(file, vertexData);
+            vector<uint8_t> labels = extract<uint8_t>(file, labelData);
+        }
         return EXIT_SUCCESS;
     } catch (exception const& e) {
         cerr << "runtime exception: " << e.what() << endl;
